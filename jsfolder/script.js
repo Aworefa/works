fa607f1eeabb8c481e8b3d7e61587d65f6d393d6
“Aworefa” <aworefatobi1999@gmail.com>
2022-09-29 06:53:52 -0400

    update on 29 sept

--- jsfolder/script.js
+++ jsfolder/script.js
@@ -1,21 +1,21 @@
-// var btn= document.getElementById('menu-btn');
-// var nav= document.getElementById('menu');
-// btn.addEventListener('click', () =>{
-//     btn.classList.toggle('open')
-//     nav.classList.toggle('flex')
-//     nav.classList.toggle('hidden')
-// })
+var btn= document.getElementById('menu-btn');
+var nav= document.getElementById('menu');
+btn.addEventListener('click', () =>{
+    btn.classList.toggle('open')
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+})
 
-// var card = document.getElementsByClassName('front')
-// let view = document.getElementById('card')
-// card.onbdlclick = over
-// function over(){
-//     view.classList.toggle('front')
-//     view.classList.toggle('back')
+var card = document.getElementsByClassName('front')
+let view = document.getElementById('card')
+card.onbdlclick = over
+function over(){
+    view.classList.toggle('front')
+    view.classList.toggle('back')
 
-// }
-document.getElementById('flip').addEventListener('click', flip);
-
-function flip() {
-    document.getElementById('card').style.display = 'hidden'
 }
+// document.getElementById('flip').addEventListener('click', flip);
+
+// function flip() {
+//     document.getElementById('card').style.display = 'hidden'
+// }
